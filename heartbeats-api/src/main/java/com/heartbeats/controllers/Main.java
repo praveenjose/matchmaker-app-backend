961eed2b9bda4f42b0b67352db40c48d4383be5a
praveenjose <praveen0471@gmail.com>
2023-12-03 11:49:27 +0530

    Initial commit

---
+++ heartbeats-api/src/main/java/com/heartbeats/controllers/Main.java
@@ -0,0 +1,7 @@
+package com.heartbeats.controllers;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ heartbeats-service/src/main/java/com/heartbeats/springboot/HeartbeatsApplication.java
@@ -0,0 +1,13 @@
+package com.heartbeats.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartbeatsApplication.class, args);
+	}
+
+}
